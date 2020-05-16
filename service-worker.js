c1786ac95f5dc95112983a1026281dd377881bd0
anastasijkar <rudich1anastasia@gmail.com>
2020-05-16 22:12:07 +0200

    update 2020: fix typos

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e94bb16f2aa6393d0541daedd780ffcc.js"
+  "/precache-manifest.7679c4d9658f5fb8b58374fe2759b577.js"
 );
 
 self.addEventListener('message', (event) => {
