8a6ac2ce76e740ff9e78cfab23d36d2ba6414f53
anastasijkar <rudich1anastasia@gmail.com>
2020-05-16 21:55:38 +0200

    update 2020

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9727ef54dfcd126f0bc6c4e738531e54.js"
+  "/precache-manifest.e94bb16f2aa6393d0541daedd780ffcc.js"
 );
 
 self.addEventListener('message', (event) => {
