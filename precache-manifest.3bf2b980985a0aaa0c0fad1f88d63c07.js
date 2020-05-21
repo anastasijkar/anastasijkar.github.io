2c06cb58254ffe571373297a5fec7d93d23934bc
anastasiia.rudych <anastasiia.rudych@likims.com>
2020-05-21 20:19:21 +0200

    2020 update

---
+++ precache-manifest.3bf2b980985a0aaa0c0fad1f88d63c07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b80405109e64dc550a4445fc3a2ecc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb006b823aa82037d4bb",
+    "url": "/static/css/main.5c5fb589.chunk.css"
+  },
+  {
+    "revision": "46292d4532abaf0d2389",
+    "url": "/static/js/2.0398aafe.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "/static/js/2.0398aafe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb006b823aa82037d4bb",
+    "url": "/static/js/main.e42cca82.chunk.js"
+  },
+  {
+    "revision": "82edbd5948c8ba1c71dd",
+    "url": "/static/js/runtime-main.e607eefe.js"
+  },
+  {
+    "revision": "e5fb7b51845f413bdc8a8366b21339a7",
+    "url": "/static/media/ph.e5fb7b51.jpg"
+  }
+]);
