8fe402513fe8246978f3b4cd7b20050c8f365d4b
Anastasiia Rudych <anastasiia.rudych@reaktor.com>
2022-12-13 13:36:22 +0000

    2023 eve update: fix typos

--- dev/src/components/organisms/Life.vue
+++ dev/src/components/organisms/Life.vue
@@ -4,10 +4,10 @@
     <div class="life__content">
       <p>I'd describe myself as a creative person who easily finds inspiration in simple things. I always try to be patient with anything in my life and keep a positive attitude.</p>
       <p>When it comes to hobbies, music takes a special place in my life. I've been a vocalist in a band for several years, then after moving abroad - done songwriting by myself and recorded music at home. Nowadays I'm more involved in another form of art - currently working on writing a novel, which has been my dream for a while.</p>
-      <p>I've been practising yoga for several years now. Last year I also picked roller skating, which I'm still learning but loving so far!</p>
+      <p>I've been practicing yoga for several years now. Last year I also picked roller skating, which I'm still learning but loving so far!</p>
       <p>Also I enjoy reading fiction books, experimenting with vegetarian cuisine, and just going on long beach walks.</p>
       <p>Currently I'm 27 y.o., originally from Kharkiv, Ukraine,
-        <br/>now living in Costa da Caparica, a beautiful portuguese oceanside village located in Lisbon Metropolitan Area.</p>
+        <br/>now living in Costa da Caparica, a beautiful Portuguese oceanside village located in Lisbon Metropolitan Area.</p>
     </div>
   </section>
 </template>
